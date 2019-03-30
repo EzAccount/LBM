ed5a0fd5a62418510b50fba548d101aa10e5d81c
Mikhail Tikhonov <misha7322@hotmail.com>
2019-03-30 21:41:57 +0300

    Minor update for collector script

--- collect.py
+++ collect.py
@@ -1,10 +1,9 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-Kn = "./Kn_pump.o";
-for i in np.arange(2, 3,1):
-    for j in [0.1, 0.3, 0.8, 1, 4, 10]:
-      os.system(Kn +" "+str(5**i) +" "+str(j) );
-      wd = str(j) +"_"+  str(5**i);
-      os.system("mkdir data/"+ wd);
-      os.system("mv tmp/* data/"+ wd +"/");
+Kn = "cmake-build-debug/Kn_pump"
+for i in np.arange(50, 550, 50):
+    for j in [0.0395, 0.079, 0.158]:
+      os.system(Kn +" "+str(i) +" "+str(j) )
+      wd = "kn" + str(j) +"_"+  str(i)
+      os.system("mv tmp/Tecplot.dat data/"+ wd)
