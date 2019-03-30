126f3d56ca20889e0db248e0b6cd486e1c6891e4
Mikhail Tikhonov <misha7322@hotmail.com>
2019-03-30 23:23:33 +0300

    .dat to file

--- collect.py
+++ collect.py
@@ -6,4 +6,4 @@ for i in np.arange(50, 550, 50):
     for j in [0.0395, 0.079, 0.158]:
       os.system(Kn +" "+str(i) +" "+str(j) )
       wd = "kn" + str(j) +"_"+  str(i)
-      os.system("mv tmp/Tecplot.dat data/"+ wd)
+      os.system("mv tmp/Tecplot.dat data/"+ wd+".dat")
